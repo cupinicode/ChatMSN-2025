30c134a31e40e5fc7a4b48fa3dc7f37d86574ae3
jorgecupini <jorgecupini@gmail.com>
2025-04-27 21:30:25 -0300

    Primer commit CHAT MSN

---
+++ public/js/index.js
@@ -0,0 +1,48 @@
+// Websocket del lado del CLIENTE
+const socket = io(); // Conexión al servidor de WebSocket
+// iniciamos la conexión desde el cliente
+// socket es una instancia de la clase Socket que representa la conexión entre el cliente y el servidor de websockets
+
+const main = () => {
+    //ocket.emit('new message', { message: 'Saludos desde el cliente'}); // Emitimos un evento. Enviamos el mensaje al servidor de WebSocket
+    socket.on('welcome', (data) => { // Escuchamos el evento 'welcome' que se emite desde el servidor
+        console.log(data); // Imprimimos el mensaje de bienvenida en la consola
+    });
+};
+
+
+// Formulario
+
+const formChat = document.getElementById('formChat'); // Obtenemos el formulario por su id
+// const formChat = document.querySelector('#formChat'); // Otra forma de obtener el formulario por su id
+const inoutUserName = document.getElementById('inputUserName'); // Obtenemos el input por su id
+const inputChat = document.getElementById('inputChat'); // Obtenemos el input por su id
+formChat.addEventListener('submit', (event) => { // Agregamos un evento al formulario
+    event.preventDefault(); // Prevenimos el comportamiento por defecto del formulario (recargar la página)
+    const userName = inoutUserName.value; // Obtenemos el valor del input del formulario
+    const message = inputChat.value; // Obtenemos el valor del input del formulario
+    if (!message) return; // Si el input está vacío, no hacemos nada
+    // Enviamos el mensaje al servidor de WebSocket
+    socket.emit('new message', { message, userName }); // Emitimos el evento 'message' al servidor de WebSocket, con el texto leido del formulario
+    inputChat.value = ''; // Limpiamos el input del formulario
+    inputChat.focus(); // Enfocamos el input del formulario
+});
+
+
+// Escuchamos el evento 'Broadcast new message' que se emite desde el servidor
+socket.on('Broadcast new message', (dataMessage) => { // Escuchamos el evento 'message' que se emite desde el servidor
+    const chatBox = document.getElementById('chatBox'); // Obtenemos el elemento por su id desde el DOM
+    chatBox.innerHTML += `<p>${dataMessage.userName} - ${dataMessage.message}</p>`; 
+    //const messageElement = document.createElement('div'); // Creamos un nuevo elemento div
+    //messageElement.innerHTML = `<strong>${dataMessage.userName}</strong>: ${dataMessage.message}`; // Agregamos el mensaje al nuevo elemento div
+    //chatBox.appendChild(messageElement); // Agregamos el nuevo elemento div al chatBox
+});
+
+socket.on('message history', (messages) => { // Escuchamos el evento 'message history' que se emite desde el servidor
+    const chatBox = document.getElementById('chatBox'); // Obtenemos el elemento por su id desde el DOM
+    messages.forEach((message) => { // Iteramos sobre el array de mensajes
+        chatBox.innerHTML += `<p>${message.userName} - ${message.message}</p>`; // Agregamos el mensaje al chatBox
+    });
+});
+
+main(); // Llamamos a la función main para iniciar la aplicación
---
+++ src/app.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import http from 'http';
+import { engine } from 'express-handlebars';
+import viewsRouter from './routes/views.router.js';
+import { Server } from 'socket.io'; // Clase SERVER para crear un servidor de WebSocket
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server); // Instancia de socket.io
+
+//Todas las solicitudes de websockets se manejan con io   (socket.io)
+//y las solicitudes de http se manejan con app    (express)
+
+// Handlebars configuration
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
+
+app.use(express.static('public'));
+
+const PORT = 8080;
+
+// EndPoints
+app.use('/', viewsRouter);
+
+//persistencia de mensajes en memoria
+const messages = [];  //Array de mensajes que se van a enviar a los clientes conectados
+
+//websocket del lado del SERVIDOR
+io.on('connection', (socket) => { //socket es un onjeto que representa la conexión entre el cliente y el servidor
+  console.log('New client connected'); //io.on('connection') se ejecuta cada vez que un cliente se conecta al servidor de websockets
+  //io es el servidor de websockets y socket es la conexión entre el cliente y el servidor
+  socket.emit('welcome', { greeting: 'Welcome to the WebSocket server!' }); //Enviamos un mensaje al cliente que se acaba de conectar
+  //Hora le vamos a enviar SOLO a ese cliente, el listado de mensajes que ya tenemos en memoria
+  //Para eso usamos SOCKET en lugar de IO
+  socket.emit('message history', messages); //Enviamos el ARRAY de mensajes al cliente que se acaba de conectar
+
+  socket.on('new message', (dataMessage) => { //Escuchamos el evento 'message' que se emite desde el cliente
+    //Cuando el servidor recibe el evento 'message' desde el cliente, ejecuta la función callback
+    messages.push(dataMessage); //Agregamos el mensaje a la lista de mensajes
+    //ocket.emit('response', 'Message received!'); //Enviamos una respuesta al cliente que envió el mensaje sólamente
+    //socket.broadcast.emit('message', dataMessage); //Enviamos el mensaje a todos los clientes conectados, excepto al que lo envió
+    io.emit('Broadcast new message', dataMessage); //Enviamos el ARRAY de mensajes a todos los clientes conectados, incluyendo al que lo envió
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+
+
+
+// la variable server sólo la usamos para iniciar el servidor y para decierle que acepte solicitudes websocket
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
